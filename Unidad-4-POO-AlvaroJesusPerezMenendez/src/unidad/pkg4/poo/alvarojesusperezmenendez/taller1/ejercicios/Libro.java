bebe67f238ac3dc2534d568675b342d0de9b7afe
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-06-30 23:44:20 -0500

    feat: Alvaro Jesus Perez Menendez, Taller 1 - Ejercicio 3

--- Unidad-4-POO-AlvaroJesusPerezMenendez/src/unidad/pkg4/poo/alvarojesusperezmenendez/taller1/ejercicios/Libro.java
+++ Unidad-4-POO-AlvaroJesusPerezMenendez/src/unidad/pkg4/poo/alvarojesusperezmenendez/taller1/ejercicios/Libro.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package unidad.pkg4.poo.alvarojesusperezmenendez.taller1.ejercicios;
 
-/**
- *
- * @author Mitzuke
- */
 public class Libro {
+    String titulo;
+    String autor;
+    int numeroPaginas;
+
+    public Libro() {
+        titulo = "Desconodico";
+        autor = "Desconocido";
+        numeroPaginas = 0;
+    }
+
+    public Libro(String titulo, String autor, int numeroPaginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
+    }
+    
+    
+    
+    public void mostrarInformacion(){
+        System.out.println("Titulo: "+ titulo + ", Autor: " + autor + ", Numero de Paginas: " + numeroPaginas + ".");
+        
+    }
     
 }
