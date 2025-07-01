c7aaf91e16845b6847ae1130d47ba46a75c94a37
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-06-30 21:45:35 -0500

    feat: Alvaro Jesus Perez Menendez - taller 1, Ejercicio 1

--- Unidad-4-POO-AlvaroJesusPerezMenendez/src/unidad/pkg4/poo/alvarojesusperezmenendez/taller1/ejercicios/Libro.java
+++ Unidad-4-POO-AlvaroJesusPerezMenendez/src/unidad/pkg4/poo/alvarojesusperezmenendez/taller1/ejercicios/Libro.java
@@ -9,26 +9,5 @@ package unidad.pkg4.poo.alvarojesusperezmenendez.taller1.ejercicios;
  * @author Mitzuke
  */
 public class Libro {
-    String titulo;
-    String autor;
-    int numeroPaginas;
-
-    public Libro() {
-        titulo = "Desconodico";
-        autor = "Desconocido";
-        numeroPaginas = 0;
-    }
-
-    public Libro(String titulo, String autor, int numeroPaginas) {
-        this.titulo = titulo;
-        this.autor = autor;
-        this.numeroPaginas = numeroPaginas;
-    }
-    
-    
-    
-    public void mostrarDetalle(){
-        System.out.println("Titulo: "+ titulo + ", Autor: " + autor + ", Numero de Paginas: " + numeroPaginas + ".");
-    }
     
 }
