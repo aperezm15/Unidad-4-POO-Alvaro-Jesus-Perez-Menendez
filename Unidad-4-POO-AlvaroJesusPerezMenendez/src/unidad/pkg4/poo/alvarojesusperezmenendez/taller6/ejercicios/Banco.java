867b1b3e51a1f5051d07228dfe3532d1406dca24
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-07-01 18:03:04 -0500

    feat: Alvaro Jesus Perez Menendez, Taller 6 - Ejercicio 3

---
+++ Unidad-4-POO-AlvaroJesusPerezMenendez/src/unidad/pkg4/poo/alvarojesusperezmenendez/taller6/ejercicios/Banco.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unidad.pkg4.poo.alvarojesusperezmenendez.taller6.ejercicios;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public class Banco {
+    // Cuando hablamos de protected, es una mala practica para este tipo de datos, ya que como lo dice en el material
+    //Clases y subclases del mismo paquete pueden acceder a ella directamente, y como la contraseña, son datos
+    //sensibles que deberian estar protegidos, Para mi opinion, se deberia usar PRIVATE, ya que este tu puedes
+    //manejar el control dando get y set.
+    protected int saldo; // >> private int saldo;
+    
+}
