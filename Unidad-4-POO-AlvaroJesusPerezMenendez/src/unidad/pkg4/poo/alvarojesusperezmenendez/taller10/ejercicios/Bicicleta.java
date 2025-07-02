2981bce610316b5d4b306b7644b68b2e8df6cefa
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-07-02 18:59:19 -0500

    feat: Alvaro Jesus Perez Menendez, Taller 10 - Ejercicio 3

--- Unidad-4-POO-AlvaroJesusPerezMenendez/src/unidad/pkg4/poo/alvarojesusperezmenendez/taller10/ejercicios/Bicicleta.java
+++ Unidad-4-POO-AlvaroJesusPerezMenendez/src/unidad/pkg4/poo/alvarojesusperezmenendez/taller10/ejercicios/Bicicleta.java
@@ -15,8 +15,14 @@ public class Bicicleta extends Vehiculo {
         super(marca);
         this.marcha = marcha;
     }
-    
+    //No utilizar Override, no va a dar un error y dejara que corra el programa
+    //Pero es mala practica, porque al momento de dar errores, este se independizara
+    //y se activara como un metodo a parte, ademas es mas sencillo identificar el problema
+    //teniendo el Override.
+    @Override
     public void moverse (){
+        //esta es la firma de la clase base, el cual si no esta puesta, no hace gracia a usar el override
+        //dado que el override significa que vamos a utilizar algo de la clase base.
         super.moverse();
         System.out.println("la bicleta tiene marchas: "+marcha);
     }
