65f25cae0ebabadc6aec333d1ea514d33af7e6b4
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-07-02 16:55:43 -0500

    feat: Alvaro Jesus Perez Menendez, Taller 8, Ejercicio 2

---
+++ Unidad-4-POO-AlvaroJesusPerezMenendez/src/unidad/pkg4/poo/alvarojesusperezmenendez/taller8/ejercicios/Estudiante.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unidad.pkg4.poo.alvarojesusperezmenendez.taller8.ejercicios;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public class Estudiante extends Persona {
+    private String matricula;
+
+    public Estudiante(String nombre, int edad, String matricula) {
+        super(nombre, edad);
+        this.matricula = matricula;
+    }
+    
+    @Override
+    public void mostrarDetalle (){
+        super.mostrarDetalle();
+        System.out.println("Matricula: "+matricula);
+        
+        
+    }
+    
+    
+}
