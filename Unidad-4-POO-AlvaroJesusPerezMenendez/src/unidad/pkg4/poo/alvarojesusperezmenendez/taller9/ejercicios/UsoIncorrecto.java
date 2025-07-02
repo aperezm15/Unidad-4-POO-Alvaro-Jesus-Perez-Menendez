b91645569f8d321f877a1ec0686856386d4bf042
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-07-02 17:54:19 -0500

    feat: Alvaro Jesus Perez Menendez, Taller 9 - Ejercicio3

---
+++ Unidad-4-POO-AlvaroJesusPerezMenendez/src/unidad/pkg4/poo/alvarojesusperezmenendez/taller9/ejercicios/UsoIncorrecto.java
@@ -0,0 +1,9 @@
+/*
+Cuando utilizamos super, nos dice que estamos en un contexto donde estamos utilizando herencia si o si.
+se utiliza para referise a la implementacion de clase padre desde dentro de su clase hija.
+
+
+Si intentamos invocar un atributo privado en la clase base utilizando super, nos dara error, porque aqui
+el private del atributo no nos dejara, como habiamos visto en talleres anteriores, el private significa que
+solo es accesible desde la propia clase donde fue declarado. para esto necesitamos getters y setters.
+*/
